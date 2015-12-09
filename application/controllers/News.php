1ff405c4946629b40cf40b5f402b300499754705
unknown <nolwenn.dantec@gmail.com>
2015-12-09 17:33:37 +0100

    formulaire ko, mise en place d'un fichier de log

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -35,5 +35,35 @@ class News extends CI_Controller {
 		        $this->load->view('templates/footer');
 		}
 
+		public function create()
+		{
+				$logger = new CI_Log;
+
+				//$logger::_log_path='E:/Users/cds/Nolwenn/logs/CI/';
+				$logger->write_log($level = 'debug', 'create', $php_error = FALSE);
+			    $this->load->helper('form');
+			    $this->load->library('form_validation');
+
+			    $data['title'] = 'Create a news item';
+
+			    $this->form_validation->set_rules('title', 'Title', 'required');
+			    $this->form_validation->set_rules('text', 'Text', 'required');
+
+			    if ($this->form_validation->run() === FALSE)
+			    {
+			    	$logger->write_log($level = 'debug', 'validation', $php_error = FALSE);
+			        $this->load->view('templates/header', $data);
+			        $this->load->view('news/create');
+			        $this->load->view('templates/footer');
+
+			    }
+			    else
+			    {
+			    	$logger->write_log($level = 'debug', 'success', $php_error = FALSE);
+			        $this->news_model->set_news();
+			        $this->load->view('news/success');
+			    }
+		}
+
         
 }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,6 @@
         <body>
 
         		<h1>Header.php</h1>
-            <!--    <h1><?php echo $nom_de_la_vue; ?></h1>
-        	-->
+            
                 <h1>--------------</h1>
 
